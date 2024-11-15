bb8f9ef9c6a38d2235d2f652fe22a34a2d194bf6
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-11-15 02:18:25 -0800

    Delete ConsumingWebApis - Part 1/Part 1 directory

--- ConsumingWebApis - Part 1/Part 1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsumingWebApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
